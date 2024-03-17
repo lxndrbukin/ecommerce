038be1ef479862d119b3d58d5db66c30ea047c00
lxndrbukin <alex.bukin@hotmail.com>
2024-03-17 18:06:16 +0000

    Updating API link

--- src/store/thunks/fetchItems.js
+++ src/store/thunks/fetchItems.js
@@ -2,6 +2,8 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export const fetchItems = createAsyncThunk('catalog/fetchItems', async () => {
-  const res = await axios.get('https://lxndrbukin.github.io/apis/catalog.json');
+  const res = await axios.get(
+    'https://lxndrbukin.github.io/apis/shoppingly_catalog.json'
+  );
   return res.data;
 });
